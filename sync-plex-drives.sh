b79d8ce7a35b5ab0257a5a3511ec07dcfb04c60f
Robee <rob@mathist.net>
2025-05-31 13:48:58 -0600

    Added script to rsync plex drive to backup

--- sync-plex-drives.sh
+++ sync-plex-drives.sh
@@ -3,7 +3,7 @@ SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 LOG_FILE="$SCRIPT_DIR/logs/sync-plex-drives.log"
 
-CURRENT_TIME=$(date +%Y-%m-%d\ at\ %l:%M:%S\ %p)
+CURRENT_TIME=$(date +%Y-%m-%d\ at%l:%M:%S\ %p)
 touch "$LOG_FILE"
 echo "$CURRENT_TIME - Starting sync" | tee -a "$LOG_FILE"
 rsync -avh --exclude 'hdd*.txt' --exclude 'System Volume Information' /mnt/g/ /mnt/p/ | tee -a "$LOG_FILE"
